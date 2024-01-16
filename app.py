43be90054a94a6c2e57f1e8d3d11827c05a62026
Manisha Varshney <94631536+manisha-v@users.noreply.github.com>
2024-01-16 19:24:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key = st.secrets["API_KEY"])
+
+model = genai.GenerativeModel('gemini-pro-vision')
+def get_gemini_response(input,image):
+    if input!="":
+       response = model.generate_content([input,image])
+    else:
+       response = model.generate_content(image)
+    return response.text
+
+
+st.set_page_config(
+    page_title="ImageInsights",
+    page_icon="🌟"
+)
+st.header("✨ Image Insights ✨")
+st.markdown("""
+This app retrieves the information from the uploaded image and answers your question related to the image using Google Gemini.
+""")
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+input=st.text_input("Input Prompt (optional): ",key="input")
+
+
+submit=st.button("Give me insights of the image!")
+
+if submit or input:
+    response=get_gemini_response(input,image)
+    st.subheader("The Response is")
+    st.write(response)
+
+st.markdown("""
+---
+
+*Built with ❤️ by Manisha Varshney*
+
+[![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/manisha-v/SQL-LLM)
+""")
